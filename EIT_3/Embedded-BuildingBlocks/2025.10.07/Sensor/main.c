ccd039021a38087addeeb5d2e6d8a28f15d6dc71
Lama072004 <Mathias.Lampert.2004@gmail.com>
2025-10-14 15:03:24 +0200

    2025-10-14

---
+++ EIT_3/Embedded-BuildingBlocks/2025.10.07/Sensor/main.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:360e9db43c0472745305c91c47c8eb876627de70f8a9a0b050b2bdf7c56a01bc
+size 3087
---
+++ EIT_3/Systemprogrammierung/Übung/Übung1/main.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:532f488bc9cfa077a268550384a5b0be69efe6e5c2271d5fd606a85221be7664
+size 1268
